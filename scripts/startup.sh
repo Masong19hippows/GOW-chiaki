b1eb75906bbfb5535385f88a255c50150e389b07
Masong19hippows <garten323@gmail.com>
2026-05-09 15:56:35 -0500

    Changing start dir

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -2,4 +2,4 @@
 set -e
 
 source /opt/gow/launch-comp.sh
-launcher /opt/chiaki --appimage-extract-and-run ${CHIAKI_STARTUP_FLAGS}
+cd ~/ && launcher /opt/chiaki --appimage-extract-and-run ${CHIAKI_STARTUP_FLAGS}
